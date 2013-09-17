70a8e5a3f61d7f8008183a5bbdd6d3fb18450200
Weihong Guan <weihong.guan@gmail.com>
2013-09-17 17:04:37 +0800

    rewrite Node as class

--- test/test103.py
+++ test/test103.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import unittest
 import serial
+import datetime
 import re
 
 class Node:
@@ -8,7 +9,7 @@ class Node:
 		self.sp = serialport
 		self.address = address
 
-	def command10(self, function):
+	def frame10(self, function):
 		s = bytearray()
 		s.append(0x10)
 		s.append(function)
@@ -17,6 +18,26 @@ class Node:
 		s.append(0x16)
 		return s
 
+	def initFrame68(self, function):
+		s = bytearray()
+		s.append(0x68)
+		s.append(0)
+		s.append(0)
+		s.append(0x68)
+		s.append(function)
+		s.append(self.address)
+		return s
+
+	def appendFrame68(self, s, asdu):
+		s.extend(asdu)
+		return s
+
+	def completeFrame68(self, s):
+		s.append(sum(s[4:]) & 0xff)
+		s.append(0x16)
+		s[1] = s[2] = len(s) - 6 
+		return s
+	
 	@staticmethod
 	def getHex(ba):
 		return " ".join("{:02x}".format(k) for k in ba)
@@ -26,7 +47,34 @@ class Node:
 
 	def close(self):
 		self.sp.close()
-		
+
+	def openCover(self):
+		s = self.initFrame68(0x53)
+		s = self.appendFrame68(s, bytearray.fromhex("40 01 0c 01 12 70 00 00"))
+		s = self.completeFrame68(s)
+		return s
+	
+	def closeCover(self):
+		s = self.initFrame68(0x53)
+		s = self.appendFrame68(s, bytearray.fromhex("40 01 0c 01 12 71 00 00"))
+		s = self.completeFrame68(s)
+		return s
+
+	def transmit(self, s):
+		self.sp.write(s)
+
+	def receive(self, n):
+		return (bytearray)(self.sp.read(n))
+
+	def adjustTime(self, t = datetime.datetime.now()):
+		s = self.initFrame68(0x44)
+		s = self.appendFrame68(s, bytearray.fromhex("06 81 08 FF FF 00"))
+		t = datetime.datetime.now()
+		millis = t.second * 1000 + t.microsecond/ 1000 
+		s = self.appendFrame68(s, bytearray([millis % 256, millis / 256, t.minute, t.hour, t.day, t.month, t.year & 0x7f]))
+		s = self.completeFrame68(s)
+		return s
+
 
 class SimplesticTest(unittest.TestCase):
 
@@ -36,19 +84,19 @@ class SimplesticTest(unittest.TestCase):
 		sp.baudrate = 9600 
 		sp.parity = serial.PARITY_EVEN
 		sp.timeout = 0.06
+
 		self.node = Node(sp, 0x01)
 		self.node.open()
-		print Node.getHex(self.node.command10(0x28))
 
 	def tearDown(self):
 		self.node.close()
 
-	def transmit(self, s):
-		self.sp.write(bytearray.fromhex(s))
-		print "\n> " + s 
+	def testPortOpen(self):
+		self.assertTrue(self.node.sp.isOpen())
 
-	def receive(self, n):
-		m = memoryview(self.sp.read(n)).tolist()
+	def command(self, outstr, instr):
+		self.node.transmit(outstr)
+		m = self.node.receive(32)
 
 		if len(m):
 			self.assertTrue(m[0] == 0x68 or m[0] == 0x10)
@@ -63,30 +111,20 @@ class SimplesticTest(unittest.TestCase):
 			if m[0] == 0x10:
 				self.assertTrue(sum(m[1:3]) & 0xff == m[-2])
 
-		s = " ".join("{:02x}".format(k) for k in m)
-		print '< ' + s
-		return s
-
-	def testPortOpen(self):
-		self.assertTrue(self.node.sp.isOpen())
-
-	def command(self, outstr, instr):
-		self.transmit(outstr)
-		s = self.receive(32)
-		#self.assertTrue(s == instr.lower())
-		self.assertTrue(re.match(instr.lower(), s))
+		self.assertTrue(re.match(instr.lower(), Node.getHex(m)))
 
 	def testOpenCover(self):
-		self.command("68 0a 0a 68 53 01 40 01 0c 01 12 70 00 00 24 16", "^10 28 01 29 16$");
+		self.command(self.node.openCover(), "^10 28 01 29 16$")
 
 	def testCloseCover(self):
-		self.command("68 0a 0a 68 53 01 40 01 0c 01 12 71 00 00 25 16", "^10 28 01 29 16$");
-
-	def testTiming(self):
-		self.command("68 0F 0F 68 44 01 06 81 08 FF FF 00 40 9C 00 12 07 08 5D 2C 16", "");
+		self.command(self.node.closeCover(), "^10 28 01 29 16$")
 
 	def testInquireCover(self):
-		self.command("10 5B 01 5C 16", "^68 1a 1a 68 08 01 32 83 00 00 0c 01 01 08 ([\da-f]{2} ){4}01 09 ([\da-f]{2} ){4}01 01 ([\da-f]{2} ){5}16$")
+		self.command(self.node.frame10(0x5b), "^68 1a 1a 68 08 01 32 83 00 00 0c 01 01 08 ([\da-f]{2} ){4}01 09 ([\da-f]{2} ){4}01 01 ([\da-f]{2} ){5}16$")
+
+	def testTiming(self):
+		self.node.adjustTime()
+		self.command(self.node.adjustTime(), "")
 
 if __name__ == '__main__':
 	unittest.main()
