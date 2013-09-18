bdee3f45fc60569c20d3f83370ce68fb7f8cff56
Weihong Guan <weihong.guan@gmail.com>
2013-09-18 10:03:00 +0800

    update test script for real case

--- test/test103.py
+++ test/test103.py
@@ -3,6 +3,7 @@ import unittest
 import serial
 import datetime
 import re
+import time
 
 class Node:
 	def __init__(self, serialport, address):
@@ -82,22 +83,25 @@ class SimplesticTest(unittest.TestCase):
 		sp = serial.Serial();
 		sp.port = '/dev/ttyUSB0'
 		sp.baudrate = 9600 
-		sp.parity = serial.PARITY_EVEN
+		sp.parity = serial.PARITY_NONE
 		sp.timeout = 0.06
 
-		self.node = Node(sp, 0x01)
+		self.node = Node(sp, 0x08)
 		self.node.open()
 
 	def tearDown(self):
 		self.node.close()
+		print
 
 	def testPortOpen(self):
 		self.assertTrue(self.node.sp.isOpen())
 
 	def command(self, outstr, instr):
 		self.node.transmit(outstr)
+		print "\n> " + Node.getHex(outstr)
+		print "= " + instr
 		m = self.node.receive(32)
-
+		print "< " + Node.getHex(m)
 		if len(m):
 			self.assertTrue(m[0] == 0x68 or m[0] == 0x10)
 			self.assertTrue(m[-1] == 0x16)
@@ -114,15 +118,19 @@ class SimplesticTest(unittest.TestCase):
 		self.assertTrue(re.match(instr.lower(), Node.getHex(m)))
 
 	def testOpenCover(self):
-		self.command(self.node.openCover(), "^10 28 01 29 16$")
+		time.sleep(0.5)
+		self.command(self.node.openCover(), Node.getHex(self.node.frame10(0x28)))
 
 	def testCloseCover(self):
-		self.command(self.node.closeCover(), "^10 28 01 29 16$")
+		time.sleep(0.5)
+		self.command(self.node.closeCover(), Node.getHex(self.node.frame10(0x28)))
 
 	def testInquireCover(self):
-		self.command(self.node.frame10(0x5b), "^68 1a 1a 68 08 01 32 83 00 00 0c 01 01 08 ([\da-f]{2} ){4}01 09 ([\da-f]{2} ){4}01 01 ([\da-f]{2} ){5}16$")
+		time.sleep(0.5)
+		self.command(self.node.frame10(0x5b), "68 1a 1a 68 08 %02x 32 83 00 00 0c 01 01 08 ([\da-f]{2} ){4}01 09 ([\da-f]{2} ){4}01 01 ([\da-f]{2} ){5}16" % (self.node.address))
 
 	def testTiming(self):
+		time.sleep(0.5)
 		self.node.adjustTime()
 		self.command(self.node.adjustTime(), "")
 
